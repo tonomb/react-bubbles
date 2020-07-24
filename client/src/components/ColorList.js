09918c1d2c22340a125391931e65c749a9f092b7
antonio martinez baez <tono.mtzb@gmail.com>
2020-07-24 11:36:31 -0500

    added add color functionality

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import axios from "axios";
 import {axiosWithAuth} from '../utils/axiosWithAuth'
 
 const initialColor = {
@@ -9,7 +8,9 @@ const initialColor = {
 
 const ColorList = ({ colors, updateColors, setUpdatedColor }) => {
   const [editing, setEditing] = useState(false);
+  const [adding, setAdding] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
+  const [colorToAdd, setColorToAdd] = useState(initialColor);
 
   const editColor = color => {
     setEditing(true);
@@ -39,11 +40,10 @@ const ColorList = ({ colors, updateColors, setUpdatedColor }) => {
 
   const deleteColor = color => {
     // make a delete request to delete this color
-    console.log('Deleting', color);
     axiosWithAuth()
       .delete(`/api/colors/${color.id}`)
       .then(res =>{
-        console.log('deleted color');
+        console.log('deleted color', color.color);
         const newColors = colors.filter(color => color.id !== res.data)
         updateColors(newColors)
       })
@@ -52,9 +52,28 @@ const ColorList = ({ colors, updateColors, setUpdatedColor }) => {
       })
   };
 
+  const addColor =(e)=>{
+    e.preventDefault()
+    if(colorToAdd.color === '' || colorToAdd.code.hex ===''){
+      setAdding(false)
+    } else{
+      axiosWithAuth()
+      .post('/api/colors', colorToAdd)
+      .then(res =>{
+        updateColors(res.data)
+        setAdding(false)
+        setColorToAdd(initialColor)
+      })
+      .catch(err =>{
+        console.log(err);
+      })
+    }
+  }
+
   return (
     <div className="colors-wrap">
       <p>colors</p>
+      <h3 className='add-color' onClick={()=>setAdding(true)}>+</h3>
       <ul>
         {colors.map(color => (
           <li key={color.color} onClick={() => editColor(color)}>
@@ -97,6 +116,36 @@ const ColorList = ({ colors, updateColors, setUpdatedColor }) => {
                 })
               }
               value={colorToEdit.code.hex}
+            />
+          </label>
+          <div className="button-row">
+            <button type="submit">save</button>
+            <button onClick={() => setAdding(false)}>cancel</button>
+          </div>
+        </form>
+      )}
+      {adding && (
+        <form onSubmit={addColor}>
+          <legend>Add color</legend>
+          <label>
+            color name:
+            <input
+              onChange={e =>
+                setColorToAdd({ ...colorToAdd, color: e.target.value })
+              }
+              value={colorToAdd.color}
+            />
+          </label>
+          <label>
+            hex code:
+            <input
+              onChange={e =>
+                setColorToAdd({
+                  ...colorToAdd,
+                  code: { hex: e.target.value }
+                })
+              }
+              value={colorToAdd.code.hex}
             />
           </label>
           <div className="button-row">
